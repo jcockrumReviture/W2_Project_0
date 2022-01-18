7475d485f02f2c235b5447cd5298d309e923ea4f
John Cockrum <johncockrum1@gmail.com>
2022-01-18 12:15:45 -0800

    refinement

--- TS_2_Punch.scala
+++ TS_2_Punch.scala
@@ -1,13 +1,12 @@
 class TS_2A_InPunch {
 
- // 1 Enter Punch
+/* Print To Screen */
+// "Today is "{Date = Now}
+// "The time is "{Time = Time of entry to screen}
+// "You are clocked " + {State} 
+// "Press 1 to Enter Punch, 2 to exit"
+    //IF ioCapture <> 1 Then RetMain
     //test state - Read State from DB {Date = Now, State}
     //IF State <> in, - write to DB -> {Method = Punch, State = in,Date =now, Time = now}
     //Else - write to DB -> {Method = Punch, State = out,Date=now, Time = (round(Now)}
 }
-
-/* 
-NOTES
-    TBD: Would this be Instance for a Case Class?
-
-*/
