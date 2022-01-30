5f96bf66195082a9bf45d7bf861e3fb5c156863f
John Cockrum <johncockrum1@gmail.com>
2022-01-30 13:38:59 -0800

    Prep

--- Main.scala
+++ Main.scala
@@ -1,11 +1,12 @@
+import java.sql._
+import java.time._
+import scala.util.Try
+import scala.io.StdIn._
+
 object Main extends App {
 
 
 
-
-
-  println("Hello, World!")
-
-
+  println("Testing")
   
 }
