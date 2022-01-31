1ff4a4e166968b45cd34135e8bba4d54c1b6a91f
John Cockrum <johncockrum1@gmail.com>
2022-01-30 22:03:06 -0800

    working ts3

--- TS_3_Hours.scala
+++ TS_3_Hours.scala
@@ -5,35 +5,32 @@ import scala.util.Try
 import scala.io.StdIn._
 
 class TS_3_InHours(val theUID: Int, val theDate: String) {
+    var changeLoop   : Boolean = true
+    var ts3_sel      : Int = 1
+    var mod_pid      : Int = 0
+    var mod_ptm      : String = ""
 
-     var thePunchCnt =0
 
+//Loopit
+    while(changeLoop){
 // Show all the punches for the date
-    getAllPunches
-
-
-//select a punch and change it
-    thePunchCnt match {
-        case 0  => {
-            cngAPunch
-            println("You have changed your first Punch ")
+        getAllPunches
+        println()
+        println("1 Edit A punch")
+        println("2 Exit ")
+        ts3_sel = readLine("Please Choose: ").toInt
+    //select a punch and change it
+        ts3_sel match {
+            case 1  => {
+                mod_pid = readLine("Please Enter Punch ID: ").toInt
+                mod_ptm = readLine("Please Enter Punch time: ").toString
+                cngAPunch
+                println(s"You have changed punch $mod_pid ")
+            }
+            case 2  => changeLoop = false
+            case _  => println("I'm sorry, I don't understand what you want to do")
         }
-        case 1  => {
-            cngAPunch
-            println("You have changed your first Punch ")
-        }
-        case 2  => {
-            cngAPunch
-            println("You have changed your first Punch")
-    
-        }
-        case 3  => {
-            cngAPunch
-            println("You have changed your first Punch")
-
-        }
-        case _  => println("I'm sorry, I don't understand what you want to do")
-    } 
+    }
 
     def getAllPunches = {
         val db_addy = "jdbc:mysql://127.0.0.1:3306/w2_project_0"
@@ -45,42 +42,35 @@ class TS_3_InHours(val theUID: Int, val theDate: String) {
         val sql =  s"SELECT PunchID, TSEntryDate, TSEntryTime FROM TSPunches WHERE TSEntryDate = '$theDate' AND fk_EmpID = $theUID ;"
         //for testing:  println (s"get Count: $sql")
         Class.forName("com.mysql.cj.jdbc.Driver")
-        val connection:Try[Connection]= Try(DriverManager.getConnection(db_addy, db_usr, db_pass))
-        val statement: Try[Statement] = connection.map(_.createStatement())
-        val resultSet: Try[ResultSet] = statement.map(_.executeQuery(sql))
-        //resultSet.map(rs => while (rs.next)   println(rs.getInt(1)) ) 
-        println("Punch ID    Date     Time")
-        while ( resultSet ) {
-        val pid = resultSet.getString(1)
-        val pda = resultSet.getString(2)
-        val ptm = resultSet.getString(3)
-        println(s"$pid    $pda    $ptm")
-      }
+        val connection =  DriverManager.getConnection(db_addy, db_usr, db_pass)
+        val statement = connection.createStatement
+        val rs = statement.executeQuery(sql)
+         println("Punch ID    Date         Time")
+            while (rs.next) {
+                val pid = rs.getString("PunchID")
+                val pda = rs.getString("TSEntryDate")
+                val ptm = rs.getString("TSEntryTime")
+                println(s"$pid         $pda    $ptm")
+            }
         // Cleanup
-        resultSet.foreach(_.close())
-        statement.foreach(_.close())
-        connection.foreach(_.close())
+        connection.close()
     }
 
     def cngAPunch = {
-        /*
         val db_addy = "jdbc:mysql://127.0.0.1:3306/w2_project_0"
         // database credentials
         val db_usr  = "root"
         val db_pass = "1q2w3e4r"
         //SQL and Connection
         //// UPDATE TSPunches SET TSEntryTime = '08:00' WHERE PunchID = 363; 
-        String sql = s"UPDATE TSPunches SET TSEntryTime = '08:00' WHERE PunchID = $theUID ;"
+        String sql = s"UPDATE TSPunches SET TSEntryTime = '$mod_ptm' WHERE PunchID = $mod_pid ;"
         Class.forName("com.mysql.cj.jdbc.Driver")
-
         Connection conn = DriverManager.getConnection(db_addy, db_usr, db_pass)
         Statement stmt = conn.createStatement()
         stmt.executeUpdate(sql)
         System.out.println("Database updated successfully ")
-        */
     }
-  
-}    
+} //Fin
 /*  Behavior
     
     accept Date Value from Main Or Restart if a new date is entered
